e8675383b85685d1c7670fa71871079c2e237177
Atabey AYKUT <atabeyaykutt@gmail.com>
2025-10-23 20:25:30 +0300

    fix: disable RNReanimated iOS autolinking due to dependency conflict with RN 0.74.7

--- react-native.config.js
+++ react-native.config.js
@@ -4,6 +4,14 @@
  */
 
 module.exports = {
-  dependencies: {},
+  dependencies: {
+    // Temporarily exclude RNReanimated from iOS autolinking
+    // due to FBReactNativeSpec dependency issue with RN 0.74.7
+    'react-native-reanimated': {
+      platforms: {
+        ios: null, // Disable for iOS
+      },
+    },
+  },
   assets: ['./assets/fonts/', './assets/images/'],
 };
